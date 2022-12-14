8a944409be0b6bfcef7eca334debaea00bde7aa3
Hiram K Hackenbacker <22875818+hackenbacker@users.noreply.github.com>
2022-12-14 15:14:58 +0900

    initial commit

---
+++ Metaball/ContentView.swift
@@ -0,0 +1,23 @@
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Copyright (c) 2022 Hackenbacker.
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Metaball/MetaballApp.swift
@@ -0,0 +1,19 @@
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Copyright (c) 2022 Hackenbacker.
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+import SwiftUI
+
+@main
+struct MetaballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
